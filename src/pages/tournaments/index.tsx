31fa3d2af14857040e1e9d32046543336d8d4d7a
jorge <jorgebdelat@uber.com>
2022-08-13 22:22:32 -0400

    feat: add tournaments page

---
+++ src/pages/tournaments/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const TournamentPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Tournaments</title>
+        <meta name="description" content="Climbing Tournaments" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>tournaments</div>
+    </>
+  );
+};
+
+export default TournamentPage;
