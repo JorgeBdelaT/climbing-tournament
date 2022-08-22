63210bb75d8e4a76d7129b871cea585c4f8967e2
jorge <jorgebdelat@uber.com>
2022-08-22 19:02:29 -0400

    feat: add tooltip to toggle theme btn

--- src/components/ToggleThemeBtn/ToggleThemeBtn.tsx
+++ src/components/ToggleThemeBtn/ToggleThemeBtn.tsx
@@ -1,19 +1,36 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import {
+  ActionIcon,
+  Container,
+  Kbd,
+  Text,
+  Tooltip,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons";
+
+const TooltipLabel = (
+  <Container p={4}>
+    <Text size="sm">
+      Press <Kbd>cmd</Kbd> or <Kbd>ctrl</Kbd> + <Kbd>j</Kbd> to toggle theme.
+    </Text>
+  </Container>
+);
 
 const ToggleThemeBtn = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
   return (
-    <ActionIcon
-      color="violet"
-      radius="xl"
-      variant="light"
-      onClick={() => toggleColorScheme()}
-    >
-      {dark ? <IconSun size={18} /> : <IconMoon size={18} />}
-    </ActionIcon>
+    <Tooltip label={TooltipLabel}>
+      <ActionIcon
+        color="primary"
+        radius="xl"
+        variant="light"
+        onClick={() => toggleColorScheme()}
+      >
+        {dark ? <IconSun size={18} /> : <IconMoon size={18} />}
+      </ActionIcon>
+    </Tooltip>
   );
 };
 
