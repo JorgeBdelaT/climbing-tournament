4ff5c83852ac0060d4c81dd050ea721bcdd3a8cc
jorge <jorgebdelat@uber.com>
2022-08-23 10:10:36 -0400

    feat: add grid example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,43 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { trpc } from "../utils/trpc";
-import { Badge, Button, Card, Group, Image, Text } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Card,
+  Group,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@mantine/core";
+
+const ExampleCard = () => (
+  <Card shadow="sm" p="lg" radius="md">
+    <Card.Section>
+      <Image
+        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+        height={160}
+        alt="Norway"
+      />
+    </Card.Section>
+
+    <Group position="apart" mt="md" mb="xs">
+      <Text weight={500}>Norway Fjord Adventures</Text>
+      <Badge color="pink" variant="light">
+        On Sale
+      </Badge>
+    </Group>
+
+    <Text size="sm" color="dimmed">
+      With Fjord Tours you can explore more of the magical fjord landscapes with
+      tours and activities on and around the fjords of Norway
+    </Text>
+
+    <Button variant="light" fullWidth mt="md" radius="md">
+      Book classic tour now
+    </Button>
+  </Card>
+);
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -22,37 +58,25 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico?" />
       </Head>
-      <main>
-        <Card shadow="sm" p="lg" radius="md" sx={{ width: 400 }}>
-          <Card.Section>
-            <Image
-              src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-              height={160}
-              alt="Norway"
-            />
-          </Card.Section>
-
-          <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>Norway Fjord Adventures</Text>
-            <Badge color="pink" variant="light">
-              On Sale
-            </Badge>
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            With Fjord Tours you can explore more of the magical fjord
-            landscapes with tours and activities on and around the fjords of
-            Norway
-          </Text>
-
-          <Button variant="light" fullWidth mt="md" radius="md">
-            Book classic tour now
-          </Button>
-        </Card>
-
+      <section>
+        <SimpleGrid
+          cols={3}
+          spacing="lg"
+          breakpoints={[
+            { maxWidth: "md", cols: 3, spacing: "md" },
+            { maxWidth: "sm", cols: 2, spacing: "sm" },
+            { maxWidth: "xs", cols: 1, spacing: "sm" },
+          ]}
+        >
+          <ExampleCard />
+          <ExampleCard />
+          <ExampleCard />
+          <ExampleCard />
+          <ExampleCard />
+        </SimpleGrid>
         {session && <h2>Hello {session.user?.email}</h2>}
         {data ? <p>{data.greeting}</p> : <p>Loading..</p>}
-      </main>
+      </section>
     </>
   );
 };
