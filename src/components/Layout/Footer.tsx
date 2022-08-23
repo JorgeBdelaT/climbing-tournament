295c41e65c7864b2a8415a4b5d6b6f400c5e5b31
jorge <jorgebdelat@uber.com>
2022-08-22 20:03:38 -0400

    feat: add basic footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "@mantine/core";
+
+const Footer = () => {
+  /* TODO: add link to github */
+  return (
+    <Center inline sx={{ height: "100%" }} p="xs">
+      <Text size="xs">Made with ❤️ by Jorge</Text>
+    </Center>
+  );
+};
+
+export default Footer;
