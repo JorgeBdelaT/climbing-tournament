5210ace7d891c550cae8340e7220e9b89f0f02ed
jorge <jorgebdelat@uber.com>
2022-08-24 10:26:36 -0400

    feat: add basic translations

---
+++ src/locales/en.ts
@@ -0,0 +1,24 @@
+const t = {
+  // Auth
+  signIn: "Sign in",
+  withGoogle: "with Google",
+  withFacebook: "with Facebook",
+  signOut: "Sign out",
+
+  // Footer
+  madeWith: "Made with",
+  by: "by",
+
+  // Mobile Menu
+  menu: "Menu",
+  navigation: "Navigation",
+  home: "Home",
+  tournaments: "Tournaments",
+  settings: "Settings",
+  toggleTheme: "Toggle theme",
+  account: "Account",
+  signInwithGoogle: "Sign in with Google",
+  signInwithFacebook: "Sign in with Facebook",
+};
+
+export default t;
---
+++ src/locales/es.ts
@@ -0,0 +1,24 @@
+const t = {
+  // Auth
+  signIn: "Ingresar",
+  withGoogle: "con Google",
+  withFacebook: "con Facebook",
+  signOut: "Cerrar sesión",
+
+  // Footer
+  madeWith: "Hecho con",
+  by: "por",
+
+  // Mobile Menu
+  menu: "Menu",
+  navigation: "Navegación",
+  home: "Inicio",
+  tournaments: "Torneos",
+  settings: "Configuración",
+  toggleTheme: "Cambiar tema",
+  account: "Cuenta",
+  signInwithGoogle: "Ingresar con Google",
+  signInwithFacebook: "Ingresar con Facebook",
+};
+
+export default t;
