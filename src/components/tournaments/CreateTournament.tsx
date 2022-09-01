0293c7309a9ee5607d9abc3bfcd42266a36e3dde
jorge <jorgebdelat@uber.com>
2022-08-31 20:07:00 -0400

    feat(create-tournament): add missing fields to step 1

--- src/components/tournaments/CreateTournament.tsx
+++ src/components/tournaments/CreateTournament.tsx
@@ -1,8 +1,8 @@
-import useTranslations from "@/hooks/useTranslations";
 import {
   Button,
   Code,
   createStyles,
+  FileInput,
   Group,
   Modal,
   Stack,
@@ -12,16 +12,23 @@ import {
   ThemeIcon,
   Title,
 } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { DatePicker } from "@mantine/dates";
 import { useMediaQuery } from "@mantine/hooks";
-import { IconDeviceFloppy, IconPlus, IconTrash } from "@tabler/icons";
+import {
+  IconCalendarEvent,
+  IconDeviceFloppy,
+  IconPlus,
+  IconTrash,
+  IconUpload,
+} from "@tabler/icons";
 import { useRouter } from "next/router";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC } from "react";
+
+import useCreateTournament from "@/hooks/tournaments/useCreateTournament";
+import useToggleLanguage from "@/hooks/useToggleLanguage";
+import useTranslations from "@/hooks/useTranslations";
 
 // TODO:
-// - BIG REFACTOR
-//    - useCreateTournament
-//    - add file with validators
 // - ver de usar useLocalStorage
 // - paso previo para ver si quiere usar datos previos
 
@@ -65,25 +72,7 @@ import { FC, useEffect, useRef, useState } from "react";
 - **judges:** Judge[]
 */
 
-interface CreateFormInput {
-  name?: string;
-  startTime?: string;
-  endTime?: string;
-  logo?: string;
-  location?: string;
-  organization?: string;
-}
-
 const IS_MOBILE_MEDIA_QUERY = "(max-width: 600px)";
-
-const INITIAL_VALUES: CreateFormInput = {
-  name: "",
-  startTime: undefined,
-  endTime: undefined,
-  logo: undefined,
-  location: "",
-  organization: "",
-};
 
 const useStyles = createStyles((theme) => ({
   header: {
@@ -131,14 +120,22 @@ const useStyles = createStyles((theme) => ({
     marginRight: 0,
     marginLeft: 0,
   },
+  column: {
+    flexGrow: 1,
+    width: "100%",
+
+    [theme.fn.largerThan("md")]: {
+      width: "unset",
+    },
+  },
 }));
 
+// TODO: create CreateTournamentModal.tsx file
 interface ModalTitleProps {
   reset: () => void;
-  disabledReset: boolean;
 }
 
-const ModalTitle: FC<ModalTitleProps> = ({ reset, disabledReset }) => {
+const ModalTitle: FC<ModalTitleProps> = ({ reset }) => {
   const t = useTranslations();
   const isMobile = useMediaQuery(IS_MOBILE_MEDIA_QUERY);
 
@@ -151,7 +148,6 @@ const ModalTitle: FC<ModalTitleProps> = ({ reset, disabledReset }) => {
       <Button
         leftIcon={<IconTrash size={18} />}
         variant="subtle"
-        disabled={disabledReset}
         onClick={reset}
       >
         Reset
@@ -164,188 +160,139 @@ const CreateTournament = () => {
   const router = useRouter();
   const { classes } = useStyles();
   const t = useTranslations();
+  const { currentLocale } = useToggleLanguage();
+  const { form, step, nextStep, prevStep, finalStep } = useCreateTournament();
 
-  const [active, setActive] = useState(0);
+  const isMobile = useMediaQuery(IS_MOBILE_MEDIA_QUERY);
 
-  const form = useForm({
-    initialValues: INITIAL_VALUES,
-
-    validate: (values) => {
-      if (active === 0) {
-        return {
-          name:
-            !values.name || (values.name && values.name.trim().length < 6)
-              ? "Name must include at least 6 characters"
-              : null,
-        };
-      }
-
-      // if (active === 1) {
-      //   return {
-      //     name:
-      //       values.name.trim().length < 2
-      //         ? "Name must include at least 2 characters"
-      //         : null,
-      //     email: /^\S+@\S+$/.test(values.email) ? null : "Invalid email",
-      //   };
-      // }
-
-      return {};
-    },
-  });
-
-  const mounted = useRef(false);
-
-  useEffect(() => {
-    if (!mounted.current) {
-      const storedValue = window.localStorage.getItem("new-tournament-form");
-      if (storedValue) {
-        try {
-          form.setValues(JSON.parse(storedValue));
-        } catch (e) {
-          console.log("Failed to parse stored value");
-        } finally {
-          mounted.current = true;
-        }
-      }
-    }
-  }, [form]);
-
-  useEffect(() => {
-    window.localStorage.setItem(
-      "new-tournament-form",
-      JSON.stringify(form.values)
-    );
-  }, [form.values]);
-
-  const nextStep = (n?: number) =>
-    setActive((current) => {
-      if (form.validate().hasErrors) {
-        return current;
-      }
-
-      // TODO: refactor
-      const nextStep = n ?? current + 1;
-      return current < 8 ? nextStep : current;
-    });
-
-  const prevStep = () =>
-    setActive((current) => (current > 0 ? current - 1 : current));
+  console.log(
+    form.values.startDate ? new Date(form.values.startDate) : undefined,
+    isMobile
+  );
 
   return (
-    <>
-      <Modal
-        opened
-        centered
-        overlayOpacity={0.55}
-        overlayBlur={3}
-        transition="fade"
-        transitionDuration={600}
-        transitionTimingFunction="ease"
-        onClose={() => router.push("/")}
-        title={
-          <ModalTitle disabledReset={!form.isTouched()} reset={form.reset} />
-        }
-        size="xl"
-        classNames={{
-          header: classes.header,
-          title: classes.title,
-          modal: classes.modal,
+    <Modal
+      opened
+      centered
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      transition="fade"
+      transitionDuration={600}
+      transitionTimingFunction="ease"
+      onClose={() => router.push("/")}
+      title={<ModalTitle reset={form.reset} />}
+      size="xl"
+      classNames={{
+        header: classes.header,
+        title: classes.title,
+        modal: classes.modal,
+      }}
+    >
+      <Text className={classes.description}>
+        {t.createTournamentDescription}
+      </Text>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          nextStep();
         }}
       >
-        <Text className={classes.description}>
-          {t.createTournamentDescription}
-        </Text>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            nextStep();
+        <input type="submit" hidden />
+        <Stepper
+          active={step}
+          breakpoint="md"
+          size="xs"
+          classNames={{
+            steps: classes.steps,
+            step: classes.step,
+            separator: classes.separator,
           }}
+          onStepClick={nextStep}
         >
-          <input type="submit" hidden />
-          <Stepper
-            active={active}
-            breakpoint="md"
-            size="xs"
-            classNames={{
-              steps: classes.steps,
-              step: classes.step,
-              separator: classes.separator,
-            }}
-            onStepClick={nextStep}
-          >
-            <Stepper.Step label={t.createTournamentStep1Label}>
-              <Stack spacing="md">
+          <Stepper.Step label={t.createTournamentStep1Label}>
+            <Group spacing="xl" align="flex-start">
+              <Stack spacing="md" className={classes.column}>
                 <TextInput
+                  withAsterisk
                   label="Name"
                   placeholder="Name"
                   {...form.getInputProps("name")}
                 />
                 <TextInput
+                  withAsterisk
                   label="Location"
                   placeholder="Location"
                   {...form.getInputProps("location")}
                 />
                 <TextInput
+                  withAsterisk
                   label="Organization"
                   placeholder="Organization"
                   {...form.getInputProps("organization")}
                 />
               </Stack>
-            </Stepper.Step>
 
-            <Stepper.Step label={t.createTournamentStep2Label}>
-              Step 2
-            </Stepper.Step>
+              <Stack spacing="md" className={classes.column}>
+                <DatePicker
+                  withAsterisk
+                  icon={<IconCalendarEvent size={14} />}
+                  placeholder="Pick the start day"
+                  label="Start Day"
+                  minDate={new Date()}
+                  locale={currentLocale}
+                  defaultValue={form.values.startDate}
+                  dropdownType={isMobile ? "modal" : "popover"}
+                  {...form.getInputProps("startDate")}
+                />
+                <DatePicker
+                  withAsterisk
+                  icon={<IconCalendarEvent size={14} />}
+                  placeholder="Pick the end day"
+                  label="End Day"
+                  minDate={form.values.startDate || new Date()}
+                  locale={currentLocale}
+                  defaultValue={form.values.endDate}
+                  dropdownType={isMobile ? "modal" : "popover"}
+                  {...form.getInputProps("endDate")}
+                />
+                <FileInput
+                  accept="image/png,image/jpeg"
+                  icon={<IconUpload size={14} />}
+                  placeholder="Pick a logo for your tournament"
+                  label="Tournament Logo"
+                  {...form.getInputProps("logo")}
+                />
+              </Stack>
+            </Group>
+          </Stepper.Step>
 
-            <Stepper.Step label={t.createTournamentStep3Label}>
-              Step 3
-            </Stepper.Step>
+          <Stepper.Step label={t.createTournamentStep2Label}>
+            Step 2
+          </Stepper.Step>
 
-            <Stepper.Step label={t.createTournamentStep4Label}>
-              Step 4
-            </Stepper.Step>
+          <Stepper.Completed>
+            Completed! Form values:
+            <Code block mt="xl">
+              {JSON.stringify(form.values, null, 2)}
+            </Code>
+          </Stepper.Completed>
+        </Stepper>
+      </form>
 
-            <Stepper.Step label={t.createTournamentStep5Label}>
-              Step 5
-            </Stepper.Step>
-
-            <Stepper.Step label={t.createTournamentStep6Label}>
-              Step 6
-            </Stepper.Step>
-
-            <Stepper.Step label={t.createTournamentStep7Label}>
-              Step 7
-            </Stepper.Step>
-
-            <Stepper.Step label={t.createTournamentStep8Label}>
-              Step 8
-            </Stepper.Step>
-
-            <Stepper.Completed>
-              Completed! Form values:
-              <Code block mt="xl">
-                {JSON.stringify(form.values, null, 2)}
-              </Code>
-            </Stepper.Completed>
-          </Stepper>
-        </form>
-
-        <Group position="right" mt="xl">
-          {active !== 0 && (
-            <Button variant="light" onClick={prevStep}>
-              Back
-            </Button>
-          )}
-          {active !== 8 && (
-            <Button onClick={() => nextStep()}>Next step</Button>
-          )}
-          {active === 8 && (
-            <Button leftIcon={<IconDeviceFloppy />}>{t.save}</Button>
-          )}
-        </Group>
-      </Modal>
-    </>
+      <Group position="right" mt="xl">
+        {step !== 0 && (
+          <Button variant="light" onClick={prevStep}>
+            Back
+          </Button>
+        )}
+        {step !== finalStep && (
+          <Button onClick={() => nextStep()}>Next step</Button>
+        )}
+        {step === finalStep && (
+          <Button leftIcon={<IconDeviceFloppy />}>{t.save}</Button>
+        )}
+      </Group>
+    </Modal>
   );
 };
 
